ce192a79214e857db49253886e7869d72daec16d
Jeremy Chen <jeremy.cz@gmail.com>
2021-11-28 22:52:54 +1300

    Use mutex to control writes to stdout

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
-#include <chrono>
+#include <mutex>
 
 #include "../thread_pool.hpp"
 
 void TestThreadPool() {
     ThreadPool thread_pool(5);
+
+    std::mutex cout_mutex;
+
     for (int i = 0; i < 20; ++i) {
-        std::cout << "Submitting Task " << i << std::endl;
-        thread_pool.Submit([i]() {
-            std::cout << "Starting Task " << i << std::endl;
+        {
+            std::lock_guard<std::mutex> lock(cout_mutex);
+            std::cout << std::this_thread::get_id() << " | Submitting Task " << i << std::endl;
+        }
+        
+        thread_pool.Submit([i, &cout_mutex]() {
+            {
+                std::lock_guard<std::mutex> lock(cout_mutex);
+                std::cout << std::this_thread::get_id() << " | Starting Task " << i << std::endl;
+            }
+            
             std::this_thread::sleep_for(std::chrono::seconds(1));
-            std::cout << "Finishing Task " << i << std::endl;
+
+            {
+                std::lock_guard<std::mutex> lock(cout_mutex);
+                std::cout << std::this_thread::get_id() << " | Finishing Task " << i << std::endl;
+            }
         });
     }
 
